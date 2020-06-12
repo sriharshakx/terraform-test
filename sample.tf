8aaca3b59eb8473cd7cdf8dd1efb6bb4c4f09339
KalluruS <>
2020-06-12 11:44:02 +0530

    ss

--- sample.tf
+++ sample.tf
@@ -4,6 +4,18 @@ resource "null_resource" "sample" {
   }
 }
 
+resource "null_resource" "kubeconfig" {
+  provisioner "local-exec" {
+    command = "aws eks update-kubeconfig --name dev-neo-eks  --region ap-south-1"
+  }
+}
+
+resource "null_resource" "kubectl" {
+  depends_on = [null_resource.kubeconfig]
+  provisioner "local-exec" {
+    command = "kubectl get pods --all-namespaces"
+  }
+}
 
 terraform {
   backend "s3" {
